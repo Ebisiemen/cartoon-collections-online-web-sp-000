e613eb95e751f173df303480b2e55d6a67692a0d
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-10 21:31:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  new_values 
-  array.any?{|call| 
-    if call.length < 4 
-      false 
-    else
-      true 
-    end 
-  end 
+  array.any?{ |call| call.length < 4} 
 end
 
 def find_the_cheese# code an argument here
