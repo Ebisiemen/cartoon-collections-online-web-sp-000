147c15e201e2b31c0690e19ce680302a507afe91
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-10 21:09:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |i| i.capitalize + "!"
+  planeteer_calls.map do |i| 
+    i.capitalize + "!"
   end 
 end
 
